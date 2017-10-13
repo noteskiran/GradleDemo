170a27d3401434555eb9e5c392ae1336ef96e64d
Kiran <kiranreddyamk@gmail.com>
2017-10-12 23:09:40 -0400

    Initial Commit

---
+++ src/main/java/com/bcp/KiranMain.java
@@ -0,0 +1,15 @@
+package com.bcp;
+
+public class KiranMain {
+
+	public static void main(String[] args) {
+
+		LocalTime localTime = new LocalTime();
+		
+		System.out.println("The Local Time = " + localTime);
+		
+		System.out.println("This is Kiran");
+		
+	}
+
+}
